207a4c7eaa34f64a995c3541bc8587fc9520eead
dotuongkhai191999 <74230621+tuongkhai-SPKT-IT@users.noreply.github.com>
2025-01-07 18:31:32 +0700

    bao cao chinh tri co the lay tu dong tu google drive

---
+++ constants.js
@@ -0,0 +1,26 @@
+const daibieu = {
+  link: "https://drive.google.com/drive/folders/12tD1sZWxmdSuDch-bKlI2eXZEsa61w5b?usp=sharing",
+  id: "12tD1sZWxmdSuDch-bKlI2eXZEsa61w5b",
+};
+const ctdh = {
+  link: "https://drive.google.com/drive/folders/1NjA7wekL5SCQOfFjgbZ4SrEqoJU2KXf_?usp=sharing",
+  id: "1NjA7wekL5SCQOfFjgbZ4SrEqoJU2KXf_",
+};
+const bcct = {
+  link: "https://drive.google.com/drive/folders/1RxdMrIqwRBjqyoYJsCDvEVC3rc-KbyHO?usp=sharing",
+  id: "1RxdMrIqwRBjqyoYJsCDvEVC3rc-KbyHO",
+};
+const hinhdaibieu = {
+  link: "https://drive.google.com/drive/folders/1cQfEo8Iwb6PHaqYqnmgBaz5DgtI1yQ9l?usp=sharing",
+  id: "1cQfEo8Iwb6PHaqYqnmgBaz5DgtI1yQ9l",
+};
+const tendaibieu = {
+  link: "https://drive.google.com/drive/folders/1eoWBHRwYJ_-gYYL34I_cwYem_MSwCSC8?usp=sharing",
+  id: "1eoWBHRwYJ_-gYYL34I_cwYem_MSwCSC8",
+};
+const video = {
+  link: "https://drive.google.com/drive/folders/1tWw-hSz66vY3spR53FxKDXibZxP6AZPR?usp=sharing",
+  id: "1tWw-hSz66vY3spR53FxKDXibZxP6AZPR",
+};
+
+const API_KEY = "AIzaSyBvnRqZlbvOqEYpKnf3XRmy6fzXtOEe7B0";
